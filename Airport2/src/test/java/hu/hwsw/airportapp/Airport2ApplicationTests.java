72add89bcd839f1931b22c0b59ab0d0e99d4f83a
imre_gabor <Imre.Gabor@aut.bme.hu>
2019-06-18 16:26:37 +0200

    migrate app test to JUnit5

--- Airport2/src/test/java/hu/hwsw/airportapp/Airport2ApplicationTests.java
+++ Airport2/src/test/java/hu/hwsw/airportapp/Airport2ApplicationTests.java
@@ -1,13 +1,15 @@
 package hu.hwsw.airportapp;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class Airport2ApplicationTests {
+
 
 	@Test
 	public void contextLoads() {
