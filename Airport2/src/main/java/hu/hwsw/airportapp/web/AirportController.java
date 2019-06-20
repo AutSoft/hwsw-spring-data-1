f711786767888fd0d4872df158aebd4e77d6c470
Imre Gábor <gabor@aut.bme.hu>
2019-06-20 11:34:34 +0200

    implement DELETE for airport

--- Airport2/src/main/java/hu/hwsw/airportapp/web/AirportController.java
+++ Airport2/src/main/java/hu/hwsw/airportapp/web/AirportController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,10 +16,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import hu.hwsw.airportapp.mapper.AirportMapper;
 import hu.hwsw.airportapp.model.Airport;
+import hu.hwsw.airportapp.repository.AirportRepository;
 import hu.hwsw.airportapp.service.AirportService;
 import hu.hwsw.airportapp.web.dto.airport.AirportDTO;
 import hu.hwsw.airportapp.web.dto.airport.NewAirportDTO;
@@ -28,37 +31,50 @@ import hu.hwsw.airportapp.web.dto.airport.NewAirportDTO;
 public class AirportController {
 
 	private final AirportService airportService;
+	private final AirportRepository airportRepository;
 	
-	public AirportController(AirportService airportService) {
+	
+	public AirportController(AirportService airportService, AirportRepository airportRepository) {
+		super();
 		this.airportService = airportService;
+		this.airportRepository = airportRepository;
 	}
 
-	
+
 	@GetMapping
 	List<AirportDTO> getAirports(@RequestParam(name = "iata", required = false) String iata, Pageable pageable) {
-		return airportService.getAirports(iata, pageable).stream().map(airport -> mapAirportToDto(airport)).collect(Collectors.toList());
+		return airportService.getAirports(iata, pageable)
+				.stream()
+				.map(airport -> mapAirportToDto(airport))
+				.collect(Collectors.toList());
 	}
 
 
 	private AirportDTO mapAirportToDto(Airport airport) {
 		return new AirportDTO(airport.getId(), airport.getCreatedAt(), airport.getModifiedAt(), airport.getName(), airport.getIata());
 	}
-	
+
 	@PostMapping
 	AirportDTO createAirport(@RequestBody @Valid NewAirportDTO newAirport) {
 		return mapAirportToDto(airportService.createAirport(newAirport));
 	}
-	
+
 	@GetMapping("/{id}")
 	ResponseEntity<AirportDTO> getAirportById(@PathVariable Long id) {
 	    return ResponseEntity
 	    		.status(HttpStatus.OK)
 	    		.body(mapAirportToDto(airportService.getAirportById(id)));
-	    
+
 	}
-	
+
 	@PutMapping("/{id}")
 	AirportDTO updateAirport(@PathVariable Long id, @RequestBody @Valid NewAirportDTO newAirport) {
 		return AirportMapper.INSTANCE.airportToDto(airportService.updateAirport(id, newAirport));
+	}
+
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void deleteAirport(@PathVariable Long id) {
+		airportRepository.deleteById(id);
 	}
 }
